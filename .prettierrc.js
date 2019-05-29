b49526cc4907d4dfdc25a68beed942d017b226c5
Samuel Brehm <samuelbrehm@gmail.com>
2019-05-29 15:40:05 -0300

    Prettier config fix

---
+++ .prettierrc.js
@@ -0,0 +1,13 @@
+module.exports = {
+  trailingComma: 'es5',
+  singleQuote: true,
+  printWidth: 80,
+  tabWidth: 2,
+  useTabs: false,
+  semi: true,
+  jsxSingleQuote: true,
+  bracketSpacing: true,
+  jsxBracketSameLine: false,
+  arrowParens: 'avoid',
+  endOfLine: 'auto',
+};
