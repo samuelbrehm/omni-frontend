284e830b05ec07fe4bea8ccf214c5ee41d688b6c
Samuel Brehm <samuelbrehm@gmail.com>
2019-06-04 14:28:30 -0300

    Routes redux

--- src/store/index.js
+++ src/store/index.js
@@ -1,14 +1,19 @@
 import { createStore, applyMiddleware } from 'redux';
 import createsagaMiddleware from 'redux-saga';
+import { routerMiddleware } from 'connected-react-router';
 
+import history from '~/routes/history';
 import rootReducer from './ducks';
 import rootSaga from './sagas';
 
 const sagaMiddleware = createsagaMiddleware();
 
-const middlewares = [sagaMiddleware];
+const middlewares = [sagaMiddleware, routerMiddleware(history)];
 
-const store = createStore(rootReducer, applyMiddleware(...middlewares));
+const store = createStore(
+  rootReducer(history),
+  applyMiddleware(...middlewares)
+);
 
 sagaMiddleware.run(rootSaga);
 
