46dadcbc214316d33fffa06779aa0989e4d830fc
Samuel Brehm <samuelbrehm@gmail.com>
2019-06-07 10:23:53 -0300

    Listing members

--- src/components/Members/index.js
+++ src/components/Members/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 
 import { connect } from 'react-redux';
@@ -10,32 +10,51 @@ import Modal from '~/components/Modal';
 import Button from '~/styles/components/Button';
 import { MembersList } from './styles';
 
-const Members = ({ closeMembersModal }) => (
-  <Modal size='big'>
-    <h1>Membros</h1>
+class Members extends Component {
+  static propTypes = {
+    closeMembersModal: PropTypes.func.isRequired,
+    getMembersRequest: PropTypes.func.isRequired,
+  };
 
-    <form>
-      <MembersList>
-        <li>
-          <strong>Samuel Brehm</strong>
-        </li>
-      </MembersList>
+  componentDidMount() {
+    const { getMembersRequest } = this.props;
 
-      <Button onClick={closeMembersModal} filled={false} color='gray'>
-        Cancelar
-      </Button>
-    </form>
-  </Modal>
-);
+    getMembersRequest();
+  }
 
-Members.propTypes = {
-  closeMembersModal: PropTypes.func.isRequired,
-};
+  render() {
+    const { closeMembersModal, members } = this.props;
+
+    return (
+      <Modal size='big'>
+        <h1>Membros</h1>
+
+        <form>
+          <MembersList>
+            {members.data.map(member => (
+              <li key={member.id}>
+                <strong>{member.user.name}</strong>
+              </li>
+            ))}
+          </MembersList>
+
+          <Button onClick={closeMembersModal} filled={false} color='gray'>
+            Cancelar
+          </Button>
+        </form>
+      </Modal>
+    );
+  }
+}
+
+const mapStateToProps = state => ({
+  members: state.members,
+});
 
 const mapDispatchToProps = dispatch =>
   bindActionCreators(MembersActions, dispatch);
 
 export default connect(
-  null,
+  mapStateToProps,
   mapDispatchToProps
 )(Members);
--- src/store/ducks/members.js
+++ src/store/ducks/members.js
@@ -5,6 +5,8 @@ import Immutable from 'seamless-immutable';
 const { Types, Creators } = createActions({
   openMembersModal: null,
   closeMembersModal: null,
+  getMembersRequest: null,
+  getMembersSuccess: ['data'],
 });
 
 export const MembersTypes = Types;
@@ -12,6 +14,7 @@ export default Creators;
 
 /* Initial State */
 export const INITIAL_STATE = Immutable({
+  data: [],
   membersModalOpen: false,
 });
 
@@ -19,8 +22,11 @@ export const INITIAL_STATE = Immutable({
 export const openModal = state => state.merge({ membersModalOpen: true });
 export const closeModal = state => state.merge({ membersModalOpen: false });
 
+export const getSuccess = (state, { data }) => state.merge({ data });
+
 /* Reducers to types */
 export const reducer = createReducer(INITIAL_STATE, {
   [Types.OPEN_MEMBERS_MODAL]: openModal,
   [Types.CLOSE_MEMBERS_MODAL]: closeModal,
+  [Types.GET_MEMBERS_SUCCESS]: getSuccess,
 });
