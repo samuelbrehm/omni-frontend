33e73d8947d16ef4551ed621ad038a49f66b7dd8
Samuel Brehm <samuelbrehm@gmail.com>
2019-06-07 11:04:21 -0300

    Editing roles members

--- src/components/Members/index.js
+++ src/components/Members/index.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+import Select from 'react-select';
+import api from '~/services/api';
 
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
@@ -16,14 +18,29 @@ class Members extends Component {
     getMembersRequest: PropTypes.func.isRequired,
   };
 
-  componentDidMount() {
+  state = {
+    roles: [],
+  };
+
+  async componentDidMount() {
     const { getMembersRequest } = this.props;
 
     getMembersRequest();
+
+    const response = await api.get('roles');
+
+    this.setState({ roles: response.data });
   }
+
+  handleRolesChange = (id, roles) => {
+    const { updateMemberRequest } = this.props;
+
+    updateMemberRequest(id, roles);
+  };
 
   render() {
     const { closeMembersModal, members } = this.props;
+    const { roles } = this.state;
 
     return (
       <Modal size='big'>
@@ -34,6 +51,14 @@ class Members extends Component {
             {members.data.map(member => (
               <li key={member.id}>
                 <strong>{member.user.name}</strong>
+                <Select
+                  isMulti
+                  options={roles}
+                  value={member.roles}
+                  getOptionLabel={role => role.name}
+                  getOptionValue={role => role.id}
+                  onChange={value => this.handleRolesChange(member.id, value)}
+                />
               </li>
             ))}
           </MembersList>
